9ef43d87e1fa9003f3415513f5f6043bf69b26f0
JHanslik <julien.hanslik@gmail.com>
2023-02-28 11:57:08 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const endpoint = "https://moigpasdamis.myshopify.com/api/2023-01/graphql.json";
+const getAllProducts = `
+{
+  products(first: 10) {
+    edges {
+      node {
+        id
+        title
+        description
+        availableForSale
+        priceRange {
+            maxVariantPrice {
+                amount
+                currencyCode
+            }
+        }
+        variants (first: 10) {
+      edges {
+          node {
+              id
+              title
+              availableForSale
+              weight
+              image {
+                  url
+              }
+              price {
+                  amount
+                  currencyCode
+              }
+          }
+      }
+    }
+      }
+    }
+  }
 }
+`;
 
-export default App;
+export default function App() {
+    const { data, isLoading, error } = useQuery("launches", () => {
+        return fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Shopify-Storefront-Access-Token":
+                    "ef6b8555f42c3e164725723455c56c43",
+            },
+            body: JSON.stringify({ query: getAllProducts }),
+        })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("Error fetching data");
+                } else {
+                    return response.json();
+                }
+            })
+            .then((data) => data.data);
+    });
+
+    if (isLoading) return "Loading...";
+    if (error) return <pre>{error.message}</pre>;
+
+    return (
+        <div>
+            <p>wololo</p>
+        </div>
+    );
+}
