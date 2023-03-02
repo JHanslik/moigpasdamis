7b28964ce18ffae0e66b57779225e9be464e76ac
JHanslik <julien.hanslik@gmail.com>
2023-03-02 10:07:40 +0100

    .env added + tailwind

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,26 +1,26 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 
 const httpLink = createHttpLink({
-    uri: "https://moigpasdamis.myshopify.com/api/2023-01/graphql.json",
-});
+  uri: process.env.REACT_APP_URI,
+})
 
 const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    // const token = localStorage.getItem("token");
-    const token = "ef6b8555f42c3e164725723455c56c43";
-    // return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            "X-Shopify-Storefront-Access-Token": token,
-        },
-    };
-});
+  // get the authentication token from local storage if it exists
+  // const token = localStorage.getItem("token");
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      "X-Shopify-Storefront-Access-Token": process.env.REACT_APP_ACCESS_TOKEN,
+    },
+  }
+})
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
-export default client;
+export default client
