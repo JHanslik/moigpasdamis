77ac0b52da42133572953c5aefaaf08c096d0cc6
JHanslik <julien.hanslik@gmail.com>
2023-03-03 14:38:33 +0100

    cart page done

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,6 +1,6 @@
 const MainContainer = ({ children }) => {
   return (
-    <main className="mt-[70px] min-h-[90vh] max-w-[1400px] mx-auto">
+    <main className="my-[90px] min-h-[90vh] max-w-[1400px] mx-auto">
       {children}
     </main>
   )
