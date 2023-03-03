ec45429e643f66a4b166f8b58f93e84b0e62f1bb
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2023-03-03 15:09:52 +0100

    orders page

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,40 +1,39 @@
-import { useQuery } from "@apollo/client";
-import { GET_COLLECTION } from "../graphql/collection/queries";
-import {useParams} from 'react-router-dom'
+import { useQuery } from "@apollo/client"
+import { GET_COLLECTION } from "../graphql/collection/queries"
+import { useParams } from "react-router-dom"
 import ProductCard from "../components/ProductCard"
 
-
 const Collection = () => {
-
   const params = useParams()
 
-    const { loading, error, data } = useQuery(GET_COLLECTION, {
-      variables: {handle: params.handle},
-    });
-    
-    
-if(loading) {
-  return(
-    <p>Loading...</p>
-    )
+  const { loading, error, data } = useQuery(GET_COLLECTION, {
+    variables: { handle: params.handle },
+  })
+
+  if (loading) {
+    return <p>Loading...</p>
   }
-  console.log(data.collectionByHandle.products.edges)
   return (
-    <> 
-    <h1 class="py-8 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Collection {data.collectionByHandle.title}</h1>
-    <p class="text-center text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{data.collectionByHandle.description} !</p>
-    <div class="flex justify-center">    
-    {data.collectionByHandle.products.edges.map((product) => {
+    <>
+      <h1 className="py-8 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Collection {data.collectionByHandle.title}
+      </h1>
+      <p className="text-center text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        {data.collectionByHandle.description} !
+      </p>
+      <div className="flex justify-center">
+        {data.collectionByHandle.products.edges.map((product) => {
           return (
             <ProductCard
-            productHandle={product.node.handle}
-            productTitle={product.node.title}
-            productCost={product.node.priceRange.maxVariantPrice.amount}
-            productImage={product.node.variants.edges[0].node.image.url}
-          />
+              key={product.node.handle}
+              productHandle={product.node.handle}
+              productTitle={product.node.title}
+              productCost={product.node.priceRange.maxVariantPrice.amount}
+              productImage={product.node.variants.edges[0].node.image.url}
+            />
           )
-    })}
-    </div>
+        })}
+      </div>
     </>
   )
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -44,28 +44,73 @@ const Profile = () => {
       setCustomerInfo(data.customerUpdate.customer)
     }
   }, [data])
-  console.log(customerInfo?.firstName)
   return (
-    <div className="flex flex-col items-center"> 
-      <div class=" mt-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 pt-10">
-          <div class="flex flex-col items-center pb-10">
-              <h5 class="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">Email</h5>
-              {!customerInfo?.email ? ('Non renseigné') : (<span class="text-sm text-gray-500 dark:text-gray-400">{customerInfo?.email}</span>)}
-              <h5 class="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">Prénom</h5>
-              {!customerInfo?.firstName ? (<span class="text-sm text-gray-500 dark:text-gray-400">Non renseigné</span>) : ( <span class="text-sm text-gray-500 dark:text-gray-400">{customerInfo?.firstName}</span>)}
-              <h5 class="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">Nom</h5>
-              {!customerInfo?.lastName ? (<span class="text-sm text-gray-500 dark:text-gray-400">Non renseigné</span>) : ( <span class="text-sm text-gray-500 dark:text-gray-400">{customerInfo.lastName}</span>)}
-              <h5 class="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">Téléphone</h5>
-              {!customerInfo?.phone ? (<span class="text-sm text-gray-500 dark:text-gray-400">Non renseigné</span>) : ( <span class="text-sm text-gray-500 dark:text-gray-400">{customerInfo.phone}</span>)}
-              <div className="flex pt-10 gap-4">
-                <Link to="/update" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Mettre à jour</Link>
-                <Link to="/orders" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Historique</Link>
-              </div>
-              
+    <div className="flex flex-col items-center">
+      <div className=" mt-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 pt-10">
+        <div className="flex flex-col items-center pb-10">
+          <h5 className="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">
+            Email
+          </h5>
+          {!customerInfo?.email ? (
+            "Non renseigné"
+          ) : (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              {customerInfo?.email}
+            </span>
+          )}
+          <h5 className="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">
+            Prénom
+          </h5>
+          {!customerInfo?.firstName ? (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Non renseigné
+            </span>
+          ) : (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              {customerInfo?.firstName}
+            </span>
+          )}
+          <h5 className="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">
+            Nom
+          </h5>
+          {!customerInfo?.lastName ? (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Non renseigné
+            </span>
+          ) : (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              {customerInfo.lastName}
+            </span>
+          )}
+          <h5 className="mt-4  mb-2 text-xl font-medium text-gray-900 dark:text-white">
+            Téléphone
+          </h5>
+          {!customerInfo?.phone ? (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Non renseigné
+            </span>
+          ) : (
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              {customerInfo.phone}
+            </span>
+          )}
+          <div className="flex pt-10 gap-4">
+            <Link
+              to="/update"
+              className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              Mettre à jour
+            </Link>
+            <Link
+              to="/orders"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              Historique
+            </Link>
           </div>
+        </div>
       </div>
     </div>
-    
   )
 }
 
